eb264adf15f2969babb8edb5d57d48aa03aa3406
seffeng <seffeng@sina.cn>
2020-05-12 10:08:29 +0800

    init

---
+++ config/sls.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+
+    /**
+     * SLS写人异常时使用该 channel 记录日志
+     */
+    'errorlogChannel' => env('SLS_ERROR_LOG_CHANNEL', 'daily'),
+
+    /**
+     * 阿里云 AccessKeyId
+     */
+    'accessKeyId' => env('SLS_ACCESS_KEY_ID', 'AccessKeyID'),
+
+    /**
+     * 阿里云 AccessKeySecret
+     */
+    'accessKeySecret' => env('SLS_ACCESS_KEY_SECRET', 'AccessKeySecret'),
+
+    /**
+     * endpoint
+     */
+    'endpoint' => env('SLS_ENDPOINT', ''),
+
+    /**
+     *  project
+     */
+    'project' => env('SLS_PROJECT', ''),
+
+    /**
+     * logStore
+     */
+    'logStore' => env('SLS_LOG_STORE', ''),
+
+    /**
+     * topic，非必须
+     */
+    'topic' => env('SLS_TOPIC', ''),
+
+    /**
+     * source，非必须
+     */
+    'source' => env('SLS_SOURCE', ''),
+
+    /**
+     * APP_ENV
+     */
+    'env' => config('app.env', ''),
+];
